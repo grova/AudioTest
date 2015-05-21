c26c18b04a4a978d3ace50726d03605553ee78de
grova <giovanni.rovatti@gmail.com>
2015-05-21 17:23:43 +0200

    attacco il play dei suoni

--- www/js/test.js
+++ www/js/test.js
@@ -1,7 +1,8 @@
-var Hammer;
+var Hammer, alert, console, Media;
 
 function Test() {
     "use strict";
+    var self = this;
     
     function init() {
         
@@ -43,16 +44,83 @@ function Test() {
             console.log("touchend");
             ev.preventDefault();
         });
+        
         $("#media_btn").on("mousedown", function () {
             console.log("down");
+            self.playMediaAudio();
+        
         });
         $("#media_btn").on("mouseup", function () {
             console.log("up");
+        });
+        $("#ll_btn").on("mousedown", function () {
+            console.log("down");
+            self.playLLAudio();
         });
         
             
 
     }
+    
+    function onSuccess() {
+        alert("media ok");
+    }
+    
+    function onError(arg) {
+        alert("media error " + arg);
+    }
+    
+    this.playMediaAudio = function () {
+        // Create Media object from src
+        var src = "./sounds/WARP.WAV",
+            my_media;
+        
+        my_media = new Media(src, onSuccess, onError);
+
+        // Play audio
+        my_media.play();
+
+        // Update my_media position every second
+        /*
+        if (mediaTimer == null) {
+            mediaTimer = setInterval(function() {
+                // get my_media position
+                my_media.getCurrentPosition(
+                    // success callback
+                    function(position) {
+                        if (position > -1) {
+                            setAudioPosition((position) + " sec");
+                        }
+                    },
+                    // error callback
+                    function(e) {
+                        console.log("Error getting pos=" + e);
+                        setAudioPosition("Error: " + e);
+                    }
+                );
+            }, 1000);
+        }
+        */
+    };
+    
+    this.playLLAudio = function () {
+        var src = "./sounds/WARP.WAV",
+            lla;
+        
+        if (window.plugins && window.plugins.LowLatencyAudio) {
+            lla = window.plugins.LowLatencyAudio;
+
+            // preload audio resource
+            lla.preloadAudio(src, src, 1, function (msg) {
+            }, function (msg) {
+                console.log('error: ' + msg);
+            });
+            
+            lla.play(src);
+        } else {
+            alert("no audio plugin");
+        }
+    };
     
     init();
 
